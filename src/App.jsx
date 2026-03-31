77c82d561ed32ec4ab9897c058c2e8dca1e14840
Justin sanchez <justin235233@gmsil.com~>
2026-03-30 23:41:42 -0400

    Add Position form UI

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import {
 } from "react-router-dom";
 import Login from "./features/Login";
 import Dashboard from "./features/Dashboard";
+import Position from "./features/Position";
 
 function App() {
   return (
@@ -16,6 +17,9 @@ function App() {
 
         {/* Ruta al Dashboard*/}
         <Route path="/dashboard" element={<Dashboard />} />
+
+        {/* Ruta Position*/}
+        <Route path="/position" element={<Position />} />
 
         {/* Redireccion por defecto */}
         <Route path="*" element={<Navigate to="/login" />} />
