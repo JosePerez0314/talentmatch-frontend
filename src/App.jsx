57ce84b57d62d8cc79d31a7e2bbd6287ed082ea1
Bryam Perez <bryamperez0710@gmail.com>
2026-04-09 19:32:32 -0400

    refactor(api): improve organization of API calls for components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-
+import React from 'react';
 // Pages
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
