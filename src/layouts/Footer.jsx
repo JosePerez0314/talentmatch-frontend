cee9c1f94f5a602a8101da215b31baf041c8f997
Justin sanchez <justin235233@gmsil.com~>
2026-04-11 22:05:44 -0400

    style: update footer background color for better UI

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className="bg-primary py-5 text-white text-[13px] font-medium flex justify-between px-10">
-        <p>Copyright {new Date().getFullYear()}</p>
-        <p>Contáctanos</p>
-        <p>Ayuda</p>
+    <footer className="bg-[#447ECA] py-5 text-white text-[13px] font-medium flex justify-between px-10">
+      <p>Copyright {new Date().getFullYear()}</p>
+      <p>Contáctanos</p>
+      <p>Ayuda</p>
     </footer>
   )
 }
