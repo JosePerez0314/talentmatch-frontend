2c3ad278e7b1bd5284fda35dbdee6c717e97b423
Bryam Perez <bryamperez0710@gmail.com>
2026-04-26 16:28:02 -0400

    feat(api): apply JWT in apiClient and fix bugs in uploads.api

--- src/services/api/uploads.api.js
+++ src/services/api/uploads.api.js
@@ -1,20 +1,13 @@
 import { apiClient } from "./apiClient";
 
 export const uploadService = {
-  // Received the userId
-  uploadCVs: (files, userId) => {
-    if (!userId) {
-      throw new Error("No se puede subir archivos sin un ID de usuario válido.");
-    }
-
+  uploadCVs: (files) => {
+    
     const formData = new FormData();
     
-    // Attach the files
+    // attach the files
     files.forEach(file => formData.append("pdfs", file));
-
-    // Attach the userId that we received from the component
-    formData.append("userId", userId);
-
+    
     return apiClient('/uploads', {
       method: 'POST',
       headers: { 'Accept': 'application/json' },
